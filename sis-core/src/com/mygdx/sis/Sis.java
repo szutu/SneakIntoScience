f241e22987d620f06de4aaff4af30ed29e1f7530
szutu <szutu@192.168.0.12>
2020-12-17 23:24:11 +0100

    17.12.2020 cd

--- sis-core/src/com/mygdx/sis/Sis.java
+++ sis-core/src/com/mygdx/sis/Sis.java
@@ -6,11 +6,13 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g3d.particles.ResourceData.AssetData;
+//import com.badlogic.gdx.graphics.g3d.particles.ResourceData.AssetData;
 import com.badlogic.gdx.graphics.g2d.BitmapFont; //
 import com.badlogic.gdx.graphics.g2d.Sprite;
 //import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Input.TextInputListener;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.math.Vector2;
 
 public class Sis extends ApplicationAdapter {
 	SpriteBatch batch;
@@ -18,8 +20,12 @@ public class Sis extends ApplicationAdapter {
 	BitmapFont font;
 	Sprite sprite;
 	TextInputListener listener;
-	Texture logo;
-	
+	Texture kropka, glowa;
+	private Array<Vector2>position;
+	private Vector2 kropkaPosition;
+	private float timer=0.1f;
+	private int num =3;
+	private boolean u,d,r=true,l; 
 	@Override
 	public void create () {
 		
@@ -32,7 +38,13 @@ public class Sis extends ApplicationAdapter {
 		sprite.setPosition(0, 0);
 		listener = new MyTextInputListener();
 		Gdx.input.getTextInput(listener,"Witaj w grze!",null,"Twoje imie");;
-	
+		glowa = new Texture("glowa.png");
+		kropka = new Texture("kropka.png");
+		position = new Array<Vector2>();
+		for (int i=0;i<num;i++) {
+			
+			position.add(new Vector2(50+i*10, 50));
+		}
 		
 		
 		
@@ -46,6 +58,18 @@ public class Sis extends ApplicationAdapter {
 		   }
 		}
 	
+	private void update(float delta) {
+		timer -=delta;
+		if(timer<=0 ) {
+			timer=0.1f;
+			movement();
+		}
+		
+	}
+	private void movement() {
+		
+		
+	}
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 1, 1, 1);
@@ -55,6 +79,12 @@ public class Sis extends ApplicationAdapter {
 		batch.end();
 		batch.begin();
 		sprite.draw(batch);
+		batch.end();
+		batch.begin();
+		for(int i=0; i<position.size;i++ ) {
+			
+			batch.draw(glowa, position.get(i).x, position.get(i).y);
+		}
 		batch.end();
 		
 	}
