906e1a59d3f8cafe7d0af153a25ff6d15d9027b4
Jakub <45537897+szutu@users.noreply.github.com>
2021-03-04 22:09:51 +0100

    Update Menu.java

--- sis-core/src/com/mygdx/sis/Menu.java
+++ sis-core/src/com/mygdx/sis/Menu.java
@@ -14,259 +14,300 @@ import com.badlogic.gdx.Input.TextInputListener;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.files.FileHandle;
 
-
+/**
+ * this class contain Menu mechanics
+ * @author Jakub Szutenberg 172434 G3
+ * 
+ */
 public class Menu extends Game {	
-	Screen MenuScreen =getScreen();
-	public String sciezka ="data/pytaniaA.txt"; // domyslnie wybrany dzial 
-	public boolean m,a,h;
-	public boolean menuDisplay =false;
-    Texture go;
-    Texture bg;
-    Texture welcome;
-    Texture instruction;
-    long delay =2000L; //opoznienie dla czekania
-    long lifeTime; //czas zycia wyswietlanego obiektu
-    protected Sprite sprite;
-    public final static String math = "Matematyka:  Wcisnij \"m\"" ;
-    public String quests=" " ;
-    public String kat;
-    public String name ="";
+    Screen MenuScreen =getScreen();
+    public String sciezka ="data/pytaniaA.txt"; // default chapter
+    public String pathAnswer1="data/odpA1.txt";
+    public String pathAnswer2="data/odpA2.txt";
+    public String path4="data/goodAnswerA.txt";
+    public final static String math = "Matematyka:  Wcisnij \"m\"" ; 
     public final static String ang = "Angielski:       Wcisnij \"a\"" ;
     public final static String his = "Historia:         Wcisnij \"h\"" ;
-    private float timer=0.1f;
-	public final static String Screen_name = "Menu";
-	public static float width = 600;
-	public static float height = 600;
-    public SpriteBatch batch;
+    public String quests=" " ;
+    public String chapter;
+    public String name ="";
+    public String currentquest[] ={"pyt1","pyt2"}; 
+    public String answerArray1[];
+    public String answerArray2[];
+    public String goodAnswer[];
+    public String answer1;
+    public String answer2;
+    public String correctAnswer;
+    public boolean mathematic,english,history; 
+    public boolean menuDisplay =false;
     protected boolean startGame;
     public boolean instrDisplay;
     public boolean goToSis;
-    public String currentquest[]; //static dodane do proby przesylu   
-    public BitmapFont font;
+    Texture chapterImage;
+    Texture bg;
+    Texture welcome;
+    Texture instruction;
     Texture img;
-    TextInputListener listener;
     double time ;
-   //Screens
-    public GameScreen gamescreen; //game screen tworzenie obiektu
-    public MenuScreen menuscreen;
+    private float timer=0.1f;
+    public static float WIDHT = 600;
+    public static float HEIGHT = 600;
+    public SpriteBatch batch;
+    protected Sprite sprite;
+    public BitmapFont font;
+    TextInputListener listener;
     //Classes
-    protected Sis game;
-    
+    public Mygame mygame;
+    public Sis game; 
+   //Screens
+    public GameScreen gamescreen; 
+    public MenuScreen menuscreen;
+    /**
+     * this method update render()
+     * @param delta: this is a value of time to be subtracted
+     */
 	public void update(float delta) { 
 		timer -=delta;
 		if(timer<=0 ) {
 			timer=0.1f;
-			// wybor();//odpowiednik funkcji movement w klasie a			
 		}
 	}
-	
-	private void showWelcome() {
-		
+	/**
+	 * this method draw welcome screen
+	 */
+	private void showWelcome() {	
 		welcome = new Texture("welcome_page.png");
 		sprite = new Sprite(welcome);
-	    	batch.begin();
-	        sprite.draw(batch);
-	        batch.end();         
+	    batch.begin();
+	    sprite.draw(batch);
+	    batch.end();         
 	}
-	
+	/**
+	 * this method draw instruction screen
+	 */
 	public void showInstruction() {
-		
 		instruction = new Texture("instrukcja.png");
 		sprite = new Sprite(instruction);   
-	    	batch.begin();
-	        sprite.draw(batch);
-	        batch.end(); 	
+	  	batch.begin();
+        sprite.draw(batch);
+        batch.end(); 	
 	}
-	
+	/**
+	 * this method listen to user inputs and determine variables values depending on the input
+	 */
 	public void input() {
 		if(Gdx.input.isKeyJustPressed(Keys.M)) {
-			m=true;
-			a=false;
-			h=false;
-			
-			 
+			mathematic=true;
+			english=false;
+			history=false;		 
 		}
 		if(Gdx.input.isKeyJustPressed(Keys.A)) {
-			m=false;
-			a=true;
-			h=false;
+			mathematic=false;
+			english=true;
+			history=false;
 		}
 		if(Gdx.input.isKeyJustPressed(Keys.H)) {
-			m=false;
-			a=false;
-			h=true;
-			
+			mathematic=false;
+			english=false;
+			history=true;
 		}
 		if(Gdx.input.isKeyJustPressed(Keys.ENTER)) {
-			startGame = true;			//do przejscia ekranow
+			startGame = true;			
 			goToSis=true;
 		}
-		if(Gdx.input.isKeyJustPressed(Keys.SPACE)) {
-			instrDisplay = true;		//do przejscia ekranow
+		if((Gdx.input.isKeyJustPressed(Keys.SPACE))&&goToSis) {
+			instrDisplay = true;	
 			goToSis=false;
 		}
 		if(Gdx.input.isKeyJustPressed(Keys.BACKSPACE)) {
-			instrDisplay=false;		   	//powrot z instrukcji do menu
+			instrDisplay=false;		   	
 			startGame=true;
-			
+			instrDisplay=false; 
+			goToSis=true;
 		}					
 	}
-	
-public void wybor() {
-		
-	if(m || a || h) {
-		
-	
-		if(m)
+	/**
+	 * this method set path to Q&A depending on input() values
+	 */
+public void select() {
+	if(mathematic || english || history) {
+		if(mathematic) 
 		{
-		 kat ="Wybrałeś kategorie: matematyka";
+		 chapter ="Wybrales kategorie: matematyka";
 		 sciezka="data/pytaniaM.txt";
-		 go = new Texture("math.png");
+		 pathAnswer1="data/odpM1.txt";
+		 pathAnswer2="data/odpM2.txt";
+		 path4="data/goodAnswerM.txt";
+		 chapterImage = new Texture("math.png");
 		 drawChapter();
 		}
-		if(a){
-			 kat ="Wybrałeś kategorie: angielski";
+		if(english){
+			 chapter ="Wybrales kategorie: angielski";
 			 sciezka="data/pytaniaA.txt";
-			 go = new Texture("english.gif");
+			 pathAnswer1="data/odpA1.txt";
+			 pathAnswer2="data/odpA2.txt";
+			 path4="data/goodAnswerA.txt";
+			 chapterImage = new Texture("english.gif");
 			 drawChapter();
 		}
-		if(h){
-			 kat ="Wybrałeś kategorie: historia";
+		if(history){
+			 chapter ="Wybrales kategorie: historia";
 			 sciezka="data/pytaniaH.txt";
-			 go = new Texture("history.gif");
-			 drawChapter();	
+			 pathAnswer1="data/odpH1.txt";
+			 pathAnswer2="data/odpH2.txt";
+			 path4="data/goodAnswerH.txt";
+			 chapterImage = new Texture("history.gif");
+			 drawChapter();	 
 		}
 	}
-	
 	else {
-		kat=" jeszcze nie wybrano kategorii";	
+		chapter=" jeszcze nie wybrano kategorii";		
 	}		
 	}
-
+	/**
+	 * this method create the object used in class Menu
+	 */
+	
     @Override
     public void create () {
-    	lifeTime = System.currentTimeMillis(); //do odliczania czasu zycia,obecnie nieuwzywane
     	batch = new SpriteBatch();
     	font = new BitmapFont();
         img = new Texture("Snake_logo.png");
-        go = new Texture("transparent.gif"); //domyslna wartosc 
+        chapterImage = new Texture("transparent.gif"); 
         bg = new Texture("bg_menu.png");   
         menuscreen=new MenuScreen(this);
         game=new Sis();
         gamescreen=new GameScreen(game);
-    	this.setScreen(menuscreen); //wywolanie screenu MenuScreen
+    	this.setScreen(menuscreen); 
     	nameInput();
-    	game.create();
-    	game.render();
+    	game.create(); 
     }
-    
-    private void nameInput() {
+    /**
+     * this method listen to user name input
+     */
+    public void nameInput() {
     	listener = new MyTextInputListener();
     	Gdx.input.getTextInput(listener,"Witaj w grze!",null,"Wpisz Swoje imie :)");
     }
+    /**
+     * this class contain method which send an input to the variable: name
+     * @author Jakub Szutenberg 172434 G3
+     *
+     */
     public class MyTextInputListener implements TextInputListener {
 		   public void input (String text) {
 			   name= text;
 		   }
-
 		   public void canceled () {
 			   return;
 		   }
 		}
-    
+    /**
+     * this method read the files specified by select() and send them to setQuestion() and setAnswer, the Sis class methods
+     */
     public void category() {	
     	FileHandle file = Gdx.files.internal(sciezka);
+    	FileHandle file1 = Gdx.files.internal(pathAnswer1);
+    	FileHandle file2 = Gdx.files.internal(pathAnswer2);
+    	FileHandle file3 = Gdx.files.internal(path4);
  	   	quests = file.readString();
+ 	   	answer1=file1.readString();
+ 	   	answer2=file2.readString();
+ 	    correctAnswer=file3.readString();
  	   	currentquest = quests.split(System.getProperty("line.separator"));
+ 	   	answerArray1=answer1.split(System.getProperty("line.separator"));
+ 	   	answerArray2=answer2.split(System.getProperty("line.separator"));
+ 	    goodAnswer=correctAnswer.split(",",13);
+ 	   	game.setQuestion(currentquest);
+ 	   	game.setAnswer(answerArray1,answerArray2,goodAnswer);
     }
-     
+     /**
+      * this method render menu on screen,in this case method render() call render() from class MenuScreen
+      */
    public void render() {
-	 	super.render(); //wywołuje przywołany tutaj: "this.setScreen(new MenuScreen(this));" screen
-	 	
-/*		poprzednie wywolanie rendera gdy nie bylo stworzonych screenow 
-		input();
-		//input2();
-		screenSwitch();
-		update(Gdx.graphics.getDeltaTime()); //CZY POTRZEBNE?		   
-*/
+	 	super.render(); 
     	}
-   
+   /**
+    * this method determine which what is visible on the screen depending on user inputs
+    */
    public void screenSwitch() {
 		if(!startGame) {
 			showWelcome();		
 			}
-		
-			if(startGame&&!instrDisplay) {
+		if(startGame&&!instrDisplay) {
 			showMenu();	
-			drawChapter();
-			wybor();	
-			}
-			
-			if (instrDisplay) {
+			drawChapter();	
+			}	
+		if (instrDisplay) {
 			showInstruction();
 			input();
-			}
-			if(instrDisplay&&goToSis) {
+			}		
+		if(instrDisplay&&goToSis) {
 			this.setScreen(gamescreen);
 			};
    }
-   
+   /**
+    * this method set the background image
+    */
    private void drawBg() {
 	    batch.begin();
 	    sprite = new Sprite(bg);
 	    sprite.setPosition(-30, 0);
 	    sprite.draw(batch);
-	    batch.end(); //tlo, wtedy glClear czyscimy
+	    batch.end(); 
    }
-   
+   /**
+    * this method calls methods which are crucial to launch menu correctly 
+    */
    public void showMenu() {	   
 	   	drawBg();
-		wybor();
+		select();
 		category();
 		showText();	
    }
-   
-   private void showText() {
+   /**
+    * this method show menu description
+    */
+   public void showText() {
 		batch.begin();
-		font.draw(batch, "Witaj w MENU, wybierz dzial: ", 220, 535);
-		font.draw(batch,math , 220, 515);
-		font.draw(batch,ang, 220, 495);
-		font.draw(batch,his, 220, 475);
-		font.draw(batch,kat, 220, 455);
-		font.draw(batch,"Witaj w grze: "+name, 220, 435);
-		//font.draw(batch,"pytania to "+quests, 220, 460); 					 stare testy
- 		//font.draw(batch,"pytanie ale z pliku "+currentquest[6], 100, 415); stare testy
-		if(a||h||m) {
-		font.draw(batch,name+"! Wcisnij SPACE aby przejsc do gry", 220, 395);
+		font.draw(batch,"Witaj w grze: "+name, 220, 530);
+		font.draw(batch, "Oto MENU, wybierz dzial: ", 220, 510);
+		font.draw(batch,math , 220,490);
+		font.draw(batch,ang, 220, 470);
+		font.draw(batch,his, 220, 450);
+		font.draw(batch,chapter, 220, 430);
+		if(english||history|mathematic) {
+		font.draw(batch,name+"! Wcisnij SPACE aby przejsc do gry", 190, 410);
 		}
 		batch.end();
    }
-   
+   /**
+    * this method show selected chapter picture, depending on user input
+    */
    public void drawChapter(){
-	
-	   sprite = new Sprite(go);
-	   if(a) {							  // dla full screenów
-		   sprite.scale((float) -0.8);    // -0.7
-		   sprite.setPosition(-200,-200); // -200,-300
+	   sprite = new Sprite(chapterImage);
+	   if(english) {					
+		   sprite.scale((float) -0.8);    
+		   sprite.setPosition(-200,-200); 
 	   }
-	   if(h) {
-		   sprite.scale((float) -0.25 );    //0.5
-		   sprite.setPosition(200,200);   //200,100
+	   if(history) {
+		   sprite.scale((float) -0.25 );    
+		   sprite.setPosition(200,200);   
 	   }
-	   if(m) {
-		   sprite.scale((float) -0.6 );    //brak 
-		   sprite.setPosition(35,150);   // brak
+	   if(mathematic) {
+		   sprite.scale((float) -0.6 );    
+		   sprite.setPosition(35,150);   
 	   }
 		batch.begin();
 		sprite.draw(batch);
 		batch.end();
    }
-   
+   /**
+    * this method dispose called methods 
+    */
     @Override
     public void dispose () {
         batch.dispose();
         font.dispose();
     }
-    
-    
 }
