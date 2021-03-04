b6b73eeaf4f50d823f632b1feefaa6b33cf5c26c
Jakub <45537897+szutu@users.noreply.github.com>
2021-03-04 22:07:05 +0100

    Update DesktopLauncher.java

--- sis-desktop/src/com/mygdx/sis/desktop/DesktopLauncher.java
+++ sis-desktop/src/com/mygdx/sis/desktop/DesktopLauncher.java
@@ -1,25 +1,18 @@
 package com.mygdx.game.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.mygdx.game.Menu;
-import com.mygdx.game.Sis;
-import com.mygdx.game.screens.MenuScreen;
-
+import com.mygdx.game.Mygame;
+/**
+ * this class Launch the game (class Mygame)
+ * @author Jakub Szutenberg 172434 G3
+ */
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		 config.resizable= false;			
 	     config.width = 600;			  
 	     config.height = 600;			
-	        
-	  // new LwjglApplication(new Sis(), config); //nieuzywane obecnie, zostaje do testowania czasem
-	     new LwjglApplication(new Menu(), config);
-	     
-
-	
-	    
-
-
-	       
+	     new LwjglApplication(new Mygame(), config);       
 	}
 }
+
