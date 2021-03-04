e28394ee5014a085c13bc6f51941ed663fe31dcd
Jakub <45537897+szutu@users.noreply.github.com>
2021-03-04 22:12:10 +0100

    Update MenuScreen.java

--- sis-core/src/com/mygdx/sis/screens/MenuScreen.java
+++ sis-core/src/com/mygdx/sis/screens/MenuScreen.java
@@ -1,6 +1,5 @@
 package com.mygdx.game.screens;
 
-
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
@@ -12,24 +11,24 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.mygdx.game.Menu;
 import com.mygdx.game.Sis;
-
+/**
+ *  this class is a screen which render whole menu 
+ * @author Jakub Szutenberg 172434 G3
+ */
 public class MenuScreen implements Screen {
-
 	protected Stage stage;
 	private OrthographicCamera camera;
 	protected SpriteBatch spriteBatch;
 	public SpriteBatch batch;
 	public BitmapFont font;
 	Texture img;
-	Texture bad;
-	//classes
-	protected Sis game;             
+	//Classes
+	public Sis game;             
 	public Menu menu;
-	
 	public MenuScreen(Menu menu) {
 		this.menu = menu;
 		createCamera();
-		stage = new Stage(new StretchViewport(Menu.width,Menu.height,camera)); 
+		stage = new Stage(new StretchViewport(Menu.WIDHT,Menu.HEIGHT,camera)); 
 		spriteBatch = new SpriteBatch();									 
 		Gdx.input.setInputProcessor(stage);									 
 	}
@@ -38,83 +37,41 @@ public class MenuScreen implements Screen {
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false,600,600);
 		camera.update();
-		
 	}
-
-	
+	/**
+	 * this method create objects used in class MenuScreen
+	 */
 	public void create() {
 		batch = new SpriteBatch();
-		menu.create();
 	}
+	/**
+	 * this method render methods imported from menu class
+	 */
 	@Override
 	public void render(float delta) {
 	    Gdx.gl.glClearColor(1, 1, 1, 0);
 	    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 	    menu.input();
 	    menu.screenSwitch();
-		update(Gdx.graphics.getDeltaTime()); //CZY POTRZEBNE?		
+		update(Gdx.graphics.getDeltaTime());
 		update(delta);;
 		camera.update();
 		spriteBatch.setProjectionMatrix(camera.combined);
 }
-	
-	
-
-	
-	//--------puste metody aby implenetowalo screen----------------------
+	//Required methods to implement screen
 	private void update(float delta) {
-		stage.act(delta);
-		
+		stage.act(delta);	
 	}
-
 	@Override
-	public void show() {
-		// TODO Auto-generated method stub
-		
-	}
-
+	public void show() {}
 	@Override
-	public void resize(int width, int height) {
-		// TODO Auto-generated method stub
-		
-	}
-
+	public void resize(int width, int height) {}
 	@Override
-	public void pause() {
-		// TODO Auto-generated method stub
-		
-	}
-
+	public void pause() {}
 	@Override
-	public void resume() {
-		// TODO Auto-generated method stub
-		
-	}
-
+	public void resume() {}
 	@Override
-	public void hide() {
-		// TODO Auto-generated method stub
-		
-	}
-
+	public void hide() {}
 	@Override
-	public void dispose() {
-		// TODO Auto-generated method stub
-		
-	}
-	//-------------------Stare metody nieyuwzywane obecnie
-	//----------------------------------------------------------------
-/*	private void clearScreen() {
-		// TODO Auto-generated method stub
-		Gdx.gl.glClearColor(1, 1, 0, 0);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-	}
-
-	private void init() {
-		bad = new Texture("badlogic.jpg"); 
-		font = new BitmapFont();
-	}
-*/
-
-
+	public void dispose() {}	
 }
